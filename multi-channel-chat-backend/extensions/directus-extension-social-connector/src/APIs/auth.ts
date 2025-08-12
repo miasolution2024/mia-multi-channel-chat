40787324fcd8ea480ddc146037bc4cf525e91067
Hieu Vo Trung <hieu.vo@idealogic.com.vn>
2025-08-12 09:15:03 +0700

    update auth url

--- multi-channel-chat-backend/extensions/directus-extension-social-connector/src/APIs/auth.ts
+++ multi-channel-chat-backend/extensions/directus-extension-social-connector/src/APIs/auth.ts
@@ -65,13 +65,11 @@ export async function handleInstagramAuthRequest(
   try {
     const redirectUri = `${integrationSettingsData.public_directus_url}/directus-extension-social-connector/api/instagram/auth/callback`;
     const instagramAuthUrl =
-      `https://api.instagram.com/oauth/authorize?` +
+      `https://www.instagram.com/oauth/authorize?` +
       `client_id=${integrationSettingsData.instagram_app_id}&` +
       `redirect_uri=${encodeURIComponent(redirectUri)}&` +
       `scope=${integrationSettingsData.instagram_scopes.join(",")}&` +
       `response_type=code`;
-
-    console.log(instagramAuthUrl);
 
     res.redirect(instagramAuthUrl);
   } catch (directusError: any) {
