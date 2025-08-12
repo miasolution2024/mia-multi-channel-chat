3ea448085a3979b5ad0864cba5d015ee4f48a50b
Hieu Vo Trung <hieu.vo@idealogic.com.vn>
2025-08-12 11:19:00 +0700

    a

--- multi-channel-chat-backend/extensions/directus-extension-social-connector/src/axios.ts
+++ multi-channel-chat-backend/extensions/directus-extension-social-connector/src/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: 'https://graph.facebook.com/v23.0' });
+const axiosInstance = axios.create({
+  baseURL: "https://graph.facebook.com/v23.0",
+});
 
 axiosInstance.interceptors.response.use(
   (response) => response,
@@ -15,11 +17,13 @@ export const IGAxiosInstance = axios.create();
 
 IGAxiosInstance.interceptors.response.use(
   (response) => response,
-  (error) =>
-    Promise.reject(
+  (error) => {
+    console.log(error);
+    return Promise.reject(
       (error.response && error.response.data && error.response.data.error) ||
         "Something went wrong!"
-    )
+    );
+  }
 );
 
 export default axiosInstance;
