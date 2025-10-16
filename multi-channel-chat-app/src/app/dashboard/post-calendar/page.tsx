bccdf92e380ebd448a8fee2140b5a46af32b0bd2
Vo Dang Trinh <vodangtrinh03@gmail.com>
2025-10-16 11:54:40 +0700

    fix: change post-calendar folders to schedule-calendar

--- multi-channel-chat-app/src/app/dashboard/post-calendar/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { PostCalendarView } from "@/sections/post-calendar/view/schedule-calendar-view";
-
-export default function PostCalendarPage() {
-  return <PostCalendarView />;
-}
