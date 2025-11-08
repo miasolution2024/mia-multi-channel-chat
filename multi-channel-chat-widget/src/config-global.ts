0eeab680ad6b4ed36a52fc69aa76c3a86def98b3
Hieu Vo Trung <hieu.vo@idealogic.com.vn>
2025-11-08 10:17:23 +0700

    move widget to seperate repo

--- multi-channel-chat-widget/src/config-global.ts
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-  serverUrl: "https://adongsilk.miasolution.io.vn",
-  websocketUrl: "wss://adongsilk.miasolution.io.vn/websocket",
-  startChatSessionWebhookUrl: "https://auto.miasolution.vn/webhook/a-dong-silk/chat-session",
-  sendMessageWebhookUrl: "https://auto.miasolution.vn/webhook/a-dong-silk/message",
-};
