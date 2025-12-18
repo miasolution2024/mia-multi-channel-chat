be819ca601eb0c7bf95fb84fb313e86958da8670
chopper999 <vanspykid85@gmail.com>
2025-12-18 20:24:21 +0700

    chore: update permission

--- multi-channel-chat-app/src/app/dashboard/layout.tsx
+++ multi-channel-chat-app/src/app/dashboard/layout.tsx
@@ -1,8 +1,6 @@
 // ----------------------------------------------------------------------
 "use client";
 import AuthGuard from "@/auth/guard/auth-guard";
-import { RoleBasedGuard } from "@/auth/guard/role-based-guard";
-import { useAuthContext } from "@/auth/hooks/use-auth-context";
 import { CONFIG } from "@/config-global";
 import { DashboardLayout } from "@/layouts/dashboard";
 
@@ -11,22 +9,20 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { user } = useAuthContext();
-
   if (CONFIG.auth.skip) {
     return <DashboardLayout>{children}</DashboardLayout>;
   }
 
   return (
     <AuthGuard>
-      <RoleBasedGuard
+      {/* <RoleBasedGuard
         hasContent
         currentRole={user?.role?.name ?? "user"}
         acceptRoles={["Administrator", "Admin", "Post social exp"]}
         sx={{ py: 10 }}
-      >
-        <DashboardLayout>{children}</DashboardLayout>
-      </RoleBasedGuard>
+      > */}
+      <DashboardLayout>{children}</DashboardLayout>
+      {/* </RoleBasedGuard> */}
     </AuthGuard>
   );
 }
